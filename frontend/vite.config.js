b7a55170f6584c418429dd129c4e870a0d3daa42
CodeDeck <codedeck@local>
2026-08-02 22:49:06 -0500

    Commit prior to user request: its not bad... heres the path to my profile pictu...

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: 3002,
     strictPort: false,
     host: true,
   },
   preview: {
-    port: 3000,
+    port: 3002,
     host: true,
   },
 })
