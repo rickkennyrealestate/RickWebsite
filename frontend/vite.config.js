1a78c3401116a960c7c219f4feb8902732a6f830
CodeDeck <codedeck@local>
2026-08-02 22:35:42 -0500

    Initial project setup

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: parseInt(process.env.PORT || '5173'),
+    port: 3000,
     strictPort: false,
     host: true,
   },
   preview: {
-    port: parseInt(process.env.PORT || '4173'),
+    port: 3000,
     host: true,
   },
 })
