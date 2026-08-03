6e4bb24935fc50d6a1c10099a856de85c88bdea6
CodeDeck <codedeck@local>
2026-08-02 23:11:40 -0500

    Commit prior to user request: https://www.facebook.com/rick.kenny.411029/ https...

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3006,
+    port: 3008,
     strictPort: false,
     host: true,
   },
   preview: {
-    port: 3006,
+    port: 3008,
     host: true,
   },
 })
