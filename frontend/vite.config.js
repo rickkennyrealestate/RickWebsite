d9aa3601c51c930628cea0c3e5b1ccd516455766
CodeDeck <codedeck@local>
2026-08-02 23:27:54 -0500

    Commit prior to user request: wait... i changed the IDX to "portal gallery" so i...

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3012,
+    port: 3014,
     strictPort: false,
     host: true,
   },
   preview: {
-    port: 3012,
+    port: 3014,
     host: true,
   },
 })
