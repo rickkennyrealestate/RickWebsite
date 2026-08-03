7fa705e6b090ab3e458fa22f0b841677cb3412be
CodeDeck <codedeck@local>
2026-08-03 00:35:26 -0500

    Commit prior to user request: it says "All Listings" but those should be "Our Li...

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3020,
+    port: 3022,
     strictPort: false,
     host: true,
   },
   preview: {
-    port: 3020,
+    port: 3022,
     host: true,
   },
 })
